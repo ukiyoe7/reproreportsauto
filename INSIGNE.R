a03fde8e976d021417b3de5423b6db64ea32929b
Sandro Jk <sandro007@gmail.com>
2022-05-31 22:24:24 -0300

    ok

--- INSIGNE.R
+++ INSIGNE.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(lubridate)
 library(googlesheets4)
+library(reshape2)
 library(DBI)
 
 con2 <- dbConnect(odbc::odbc(), "reproreplica")
@@ -17,7 +18,7 @@ insigne <- dbGetQuery(con2,"
   
   CLI AS (SELECT C.CLICODIGO,SETOR FROM CLIEN C
   LEFT JOIN (SELECT CLICODIGO, ZODESCRICAO SETOR FROM ENDCLI E
-  INNER JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA)Z ON E.ZOCODIGO=Z.ZOCODIGO
+  INNER JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA WHERE ZOCODIGO IN(20,21,22,23,24,28))Z ON E.ZOCODIGO=Z.ZOCODIGO
   WHERE ENDFAT='S'
   ) ED ON C.CLICODIGO=ED.CLICODIGO
   WHERE CLICLIENTE='S'),
@@ -26,7 +27,7 @@ insigne <- dbGetQuery(con2,"
   PED AS (SELECT ID_PEDIDO,PEDDTBAIXA,P.CLICODIGO,SETOR FROM PEDID P
   INNER JOIN FIS ON P.FISCODIGO1=FIS.FISCODIGO
    INNER JOIN CLI C ON P.CLICODIGO=C.CLICODIGO
-    WHERE PEDDTBAIXA BETWEEN '01.05.2022' AND 'YESTERDAY' AND PEDSITPED<>'C' AND PEDLCFINANC IN ('S', 'L','N')),
+    WHERE PEDDTBAIXA BETWEEN '01.01.2022' AND 'TODAY' AND PEDSITPED<>'C' AND PEDLCFINANC IN ('S', 'L','N')),
     
      PROD AS (SELECT PROCODIGO FROM PRODU WHERE MARCODIGO='189' AND PROTIPO<>'T')
   
@@ -47,3 +48,29 @@ insigne <- dbGetQuery(con2,"
 
 
 View(insigne)
+
+## QUANTIDADE
+
+
+insigne2022_q <- insigne %>% group_by(SETOR,MES=toupper(format(floor_date(PEDDTBAIXA,"month"),"%b"))) %>% 
+  summarize(QTD=round(sum(QTD),1)) %>% 
+  dcast(SETOR ~ factor(MES, levels =c("JAN","FEV","MAR","ABR","MAI","JUN","JUL","AGO",
+                                                       "SET","OUT","NOV","DEZ"))) 
+
+
+range_write("1izIb0rDEtyAh48Jtv_GkjbyXWhWEeqNTiGeKeMAoeB0",
+            data=insigne2022_q,sheet = "QTD",
+            range = "A15",reformat = FALSE)
+
+## VALOR
+
+insigne2022_v <- insigne %>% group_by(SETOR,MES=toupper(format(floor_date(PEDDTBAIXA,"month"),"%b"))) %>% 
+  summarize(VALOR=round(sum(VRVENDA),1)) %>% 
+  dcast(SETOR ~ factor(MES, levels =c("JAN","FEV","MAR","ABR","MAI","JUN","JUL","AGO",
+                                      "SET","OUT","NOV","DEZ"))) 
+
+
+range_write("1izIb0rDEtyAh48Jtv_GkjbyXWhWEeqNTiGeKeMAoeB0",
+            data=insigne2022_v,sheet = "VALOR",
+            range = "A15",reformat = FALSE)
+
